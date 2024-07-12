d800cc914225a24ff624cfb850ab18f7ac912fe3
Rodolfo Olivieri <rolivier@redhat.com>
2024-07-12 13:42:39 -0300

    Update

--- nvim/init.lua
+++ nvim/init.lua
@@ -93,6 +93,8 @@ vim.g.maplocalleader = ' '
 -- Set to true if you have a Nerd Font installed and selected in the terminal
 vim.g.have_nerd_font = true
 
+vim.cmd.colorscheme 'vim'
+
 -- [[ Setting options ]]
 require 'options'
 
