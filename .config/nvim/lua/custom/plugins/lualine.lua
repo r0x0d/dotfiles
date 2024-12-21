d8b1020b62ba0f1b37239ded4741c3a07f7cdef6
Rodolfo Olivieri <rolivier@redhat.com>
2024-12-20 21:54:27 -0300

    Update config

--- .config/nvim/lua/custom/plugins/lualine.lua
+++ .config/nvim/lua/custom/plugins/lualine.lua
@@ -1,15 +1,15 @@
 return {
-    {
-        "nvim-lualine/lualine.nvim",
-        event = "VeryLazy",
-        opts = {
-          theme = "gruvbox",
-          sections = {
-            lualine_a = { "mode" },
-            lualine_b = { "branch", "diff", "diagnostics", { "filename", file_status = false }, { modified } },
-            lualine_c = {},
-          }
-        },
-        dependencies = { "nvim-web-devicons" },
+  {
+    'nvim-lualine/lualine.nvim',
+    event = 'VeryLazy',
+    opts = {
+      sections = {
+        lualine_a = { 'mode' },
+        lualine_b = { 'branch', 'diff', 'diagnostics', { 'filename', file_status = false }, { modified } },
+        lualine_c = {},
       },
+    },
+    dependencies = { 'nvim-web-devicons' },
+  },
 }
+
