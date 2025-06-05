fe32056e2834997dad1fd5185a3a89adece0051f
Rodolfo Olivieri <rodolfo.olivieri3@gmail.com>
2025-06-05 11:26:55 -0300

    remove emacs and update nvim

--- editors/nvim/lua/custom/plugins/vague.lua
+++ editors/nvim/lua/custom/plugins/vague.lua
@@ -3,10 +3,13 @@ return { -- You can easily change to a different colorscheme.
   -- change the command in the config to whatever the name of that colorscheme is.
   --
   -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
-  'catppuccin/nvim',
+  'vague2k/vague.nvim',
   priority = 1000, -- Make sure to load this before all the other start plugins.
   init = function()
-    vim.cmd.colorscheme 'catppuccin'
+    vim.cmd.colorscheme 'vague'
+  end,
+  config = function()
+    require('vague').setup()
   end,
 }
 -- vim: ts=2 sts=2 sw=2 et
