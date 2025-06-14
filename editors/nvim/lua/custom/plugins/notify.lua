b4f560cfeb6ac2e9d6b77ef47524751c31760d81
Rodolfo Olivieri <rodolfo.olivieri3@gmail.com>
2025-06-13 22:26:34 -0300

    Update nvim deps

--- editors/nvim/lua/custom/plugins/notify.lua
+++
@@ -1,8 +0,0 @@
-return {
-  'rcarriga/nvim-notify',
-  name = 'notify',
-  init = function(plugin)
-    vim.notify = require(plugin.name)
-  end,
-  dependencies = { 'nvim-telescope/telescope.nvim' },
-}
