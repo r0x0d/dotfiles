3966e03b19f5ce37a62e8e27e48f6fdf26941a07
Rodolfo Olivieri <rodolfo.olivieri3@gmail.com>
2025-08-22 22:19:32 -0300

    Update dotfiles

--- editors/nvim/after/ftplugin/go.lua
+++ editors/nvim/after/ftplugin/go.lua
@@ -4,15 +4,15 @@ set.expandtab = false
 set.tabstop = 4
 set.shiftwidth = 4
 
-vim.keymap.set("n", "<space>td", function()
-  require("dap-go").debug_test()
+vim.keymap.set('n', '<space>td', function()
+  require('dap-go').debug_test()
 end, { buffer = 0 })
 
-local golang = vim.api.nvim_create_augroup("goformat", { clear = true })
-vim.api.nvim_create_autocmd("BufWritePre", {
-  pattern = "*.go",
+local golang = vim.api.nvim_create_augroup('goformat', { clear = true })
+vim.api.nvim_create_autocmd('BufWritePre', {
+  pattern = '*.go',
   group = golang,
   callback = function()
-    require("go.format").goimport()
+    require('go.format').goimport()
   end,
 })
