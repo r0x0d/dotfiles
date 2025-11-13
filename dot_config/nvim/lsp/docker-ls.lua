145d9c9524678f4da0fbcc5432686ee2b440fd70
Rodolfo Olivieri <rodolfo.olivieri3@gmail.com>
2025-11-13 18:50:38 -0300

    Update .config/nvim/lsp/bash-ls.lua Add .config/nvim/lsp/docker-ls.lua

---
+++ dot_config/nvim/lsp/docker-ls.lua
@@ -0,0 +1,29 @@
+---@brief
+---
+--- https://github.com/docker/docker-language-server
+---
+--- `docker-langserver-server` can be installed via `go`:
+--- ```sh
+--- go install github.com/docker/docker-language-server/cmd/docker-language-server@latest
+--- ```
+
+---@type vim.lsp.Config
+return {
+  cmd = { 'docker-language-server', 'start', '--stdio' },
+  filetypes = { 'dockerfile', 'yaml.docker-compose' },
+  get_language_id = function(_, ftype)
+    if ftype == 'yaml.docker-compose' or ftype:lower():find('ya?ml') then
+      return 'dockercompose'
+    else
+      return ftype
+    end
+  end,
+  root_markers = {
+    'Containerfile',
+    'podman-compose.ym',
+    'podman-compose.yaml',
+    'Dockerfile',
+    'docker-compose.yaml',
+    'docker-compose.yml',
+  },
+}
