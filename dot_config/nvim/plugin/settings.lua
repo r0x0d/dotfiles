42c91b588ca6a498df3738fc9061e04f1baf7129
Rodolfo Olivieri <rodolfo.olivieri3@gmail.com>
2025-11-26 22:32:47 -0300

    Update .config/nvim/lazy-lock.json Update .config/nvim/lua/plugins/quicker.lua Update .config/nvim/lua/plugins/treesitter.lua Update .config/nvim/plugin/settings.lua Update .config/nvim/snippets/make.json Update .config/nvim/snippets/python.json

--- dot_config/nvim/plugin/settings.lua
+++ dot_config/nvim/plugin/settings.lua
@@ -48,26 +48,6 @@ vim.wo.foldtext = ''
 -- Use rounded borders for floating windows.
 vim.o.winborder = 'rounded'
 
--- Sync clipboard between the OS and Neovim.
-if not vim.env.SSH_CLIENT and not vim.env.SSH_TTY then
-  vim.o.clipboard = 'unnamedplus'
-end
-
--- Use OSC 52 for SSH sessions to support clipboard sync through terminal
-if vim.env.SSH_CLIENT or vim.env.SSH_TTY then
-  vim.g.clipboard = {
-    name = 'OSC 52 terminal clipboard',
-    copy = {
-      ['+'] = require('vim.ui.clipboard.osc52').copy '+',
-      ['*'] = require('vim.ui.clipboard.osc52').copy '*',
-    },
-    paste = {
-      ['+'] = require('vim.ui.clipboard.osc52').paste '+',
-      ['*'] = require('vim.ui.clipboard.osc52').paste '*',
-    },
-  }
-end
-
 -- Save undo history.
 vim.o.undofile = true
 
