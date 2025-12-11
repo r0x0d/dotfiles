05dc6ad78cd54f9c9e71755256e3315b04442d91
Rodolfo Olivieri <rodolfo.olivieri3@gmail.com>
2025-12-10 21:28:04 -0300

    Add .config/fish/config.fish

---
+++ dot_config/private_fish/config.fish
@@ -0,0 +1,21 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+
+set -U fish_prompt_pwd_dir_length 0
+
+abbr "g" "git"
+abbr "gga" "gg amend"
+abbr "ggc" "gg commit"
+abbr "gl" "git log"
+abbr "gp" "git push"
+abbr "gpf" "git push --force-with-lease"
+abbr "gs" "git status"
+abbr "gup" "git pull --rebase"
+abbr "sw" "gg switch"
+abbr "swr" "gg switch --remote"
+abbr "swd" "git switch --detach"
+
+alias "nightmare" "ssh nightmare"
+alias "doom" "ssh doom"
+alias "redhat" "ssh redhat"
