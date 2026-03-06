200501f832f64fb3cab83fe5c999beb54fe63bb5
Rodolfo Olivieri <rodolfo.olivieri3@gmail.com>
2026-03-06 19:14:46 -0300

    Remove .config/nvim/README.md Remove .config/nvim/after/ftplugin/go.lua Remove .config/nvim/after/ftplugin/help.lua Remove .config/nvim/after/ftplugin/lua.lua Remove .config/nvim/colors/gnome.lua Remove .config/nvim/colors/kde.lua Remove .config/nvim/.stylua.toml Remove .config/nvim/init.lua Remove .config/nvim/lazy-lock.json Remove .config/nvim/lsp/bash-ls.lua Remove .config/nvim/lsp/clangd-ls.lua Remove .config/nvim/lsp/csharp-ls.lua Remove .config/nvim/lsp/docker-ls.lua Remove .config/nvim/lsp/go-ls.lua Remove .config/nvim/lsp/json-ls.lua Remove .config/nvim/lsp/lua-ls.lua Remove .config/nvim/lsp/markdown-ls.lua Remove .config/nvim/lsp/python-ls.lua Remove .config/nvim/lsp/rpm-ls.lua Remove .config/nvim/lsp/ruby-ls.lua Remove .config/nvim/lsp/ruff-ls.lua Remove .config/nvim/lsp/rust-ls.lua Remove .config/nvim/lsp/yaml-ls.lua Remove .config/nvim/lsp/zig-ls.lua Remove .config/nvim/lua/r0x0d/init.lua Remove .config/nvim/lua/r0x0d/lazy.lua Remove .config/nvim/lua/r0x0d/plugins/coding/blink.lua Remove .config/nvim/lua/r0x0d/plugins/coding/conform.lua Remove .config/nvim/lua/r0x0d/plugins/coding/init.lua Remove .config/nvim/lua/r0x0d/plugins/coding/luasnip.lua Remove .config/nvim/lua/r0x0d/plugins/coding/schemastore.lua Remove .config/nvim/lua/r0x0d/plugins/coding/yaml.lua Remove .config/nvim/lua/r0x0d/plugins/editor/init.lua Remove .config/nvim/lua/r0x0d/plugins/editor/lualine.lua Remove .config/nvim/lua/r0x0d/plugins/editor/mason.lua Remove .config/nvim/lua/r0x0d/plugins/editor/oil.lua Remove .config/nvim/lua/r0x0d/plugins/editor/quicker.lua Remove .config/nvim/lua/r0x0d/plugins/editor/sleuth.lua Remove .config/nvim/lua/r0x0d/plugins/editor/todo-comments.lua Remove .config/nvim/lua/r0x0d/plugins/editor/treesitter.lua Remove .config/nvim/lua/r0x0d/plugins/git/diffview.lua Remove .config/nvim/lua/r0x0d/plugins/git/gitlinker.lua Remove .config/nvim/lua/r0x0d/plugins/git/gitsigns.lua Remove .config/nvim/lua/r0x0d/plugins/git/init.lua Remove .config/nvim/lua/r0x0d/plugins/git/octo.lua Remove .config/nvim/lua/r0x0d/plugins/init.lua Remove .config/nvim/lua/r0x0d/plugins/tools/dap.lua Remove .config/nvim/lua/r0x0d/plugins/tools/grug-far.lua Remove .config/nvim/lua/r0x0d/plugins/tools/init.lua Remove .config/nvim/lua/r0x0d/plugins/tools/neotest.lua Remove .config/nvim/lua/r0x0d/plugins/tools/telescope.lua Remove .config/nvim/lua/r0x0d/theme/gnome/highlights.lua Remove .config/nvim/lua/r0x0d/theme/gnome/lualine.lua Remove .config/nvim/lua/r0x0d/theme/gnome/palette.lua Remove .config/nvim/lua/r0x0d/theme/kde/highlights.lua Remove .config/nvim/lua/r0x0d/theme/kde/lualine.lua Remove .config/nvim/lua/r0x0d/theme/kde/palette.lua Remove .config/nvim/lua/r0x0d/util/icons.lua Remove .config/nvim/lua/r0x0d/util/init.lua Remove .config/nvim/lua/r0x0d/util/lightbulb.lua Remove .config/nvim/plugin/autocmds.lua Remove .config/nvim/plugin/bigfile.lua Remove .config/nvim/plugin/commands.lua Remove .config/nvim/plugin/filetype.lua Remove .config/nvim/plugin/keymaps.lua Remove .config/nvim/plugin/lsp.lua Remove .config/nvim/plugin/marks.lua Remove .config/nvim/plugin/options.lua Remove .config/nvim/snippets/make.json Remove .config/nvim/snippets/package.json Remove .config/nvim/snippets/python.json Remove .config/nvim/syntax/log.vim Update .gitconfig

--- dot_config/nvim/lsp/rpm-ls.lua
+++
@@ -1,8 +0,0 @@
--- Install with: sudo dnf install rpm-spec-language-server 
-
----@type vim.lsp.Config
-return {
-    cmd = { 'rpm_lsp_server', '--stdio' },
-    filetypes = { 'spec' },
-    settings = {},
-}
--- dot_config/nvim/lua/r0x0d/plugins/editor/sleuth.lua
+++
@@ -1,6 +0,0 @@
--- Sleuth: Automatic indentation detection
-return {
-    'tpope/vim-sleuth',
-    event = { 'BufReadPre', 'BufNewFile' }, -- Load when opening files
-}
-
