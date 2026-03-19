8438d8276a329207295090219d967987faf4fc0a
Rodolfo Olivieri <rodolfo.olivieri3@gmail.com>
2026-03-19 12:00:24 -0300

    Update .config/nvim/lazy-lock.json Update .config/nvim/lua/r0x0d/plugins/tools/init.lua

--- dot_config/nvim/lua/r0x0d/plugins/tools/init.lua
+++ dot_config/nvim/lua/r0x0d/plugins/tools/init.lua
@@ -1,9 +1,7 @@
--- Tool plugins (telescope, dap, testing, etc.)
+-- Tools integration plugins
 return {
-    require 'r0x0d.plugins.tools.telescope',
     require 'r0x0d.plugins.tools.dap',
-    require 'r0x0d.plugins.tools.neotest',
     require 'r0x0d.plugins.tools.grug-far',
-    require 'r0x0d.plugins.tools.neorg',
+    require 'r0x0d.plugins.tools.neotest',
+    require 'r0x0d.plugins.tools.telescope',
 }
-
