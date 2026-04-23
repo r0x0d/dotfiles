adeb82f016475e7bdcbb90621e32f1727426744b
Rodolfo Olivieri <rolivier@redhat.com>
2026-04-23 15:52:18 -0300

    Update .config/nvim/lua/plugins/treesitter.lua

--- dot_config/nvim/lua/plugins/treesitter.lua
+++ dot_config/nvim/lua/plugins/treesitter.lua
@@ -3,20 +3,14 @@ return {
     build = ":TSUpdate",
     event = { "BufReadPost", "BufNewFile" },
     config = function()
-        local ensure = {
-            "python", "rust", "c_sharp", "gdscript",
-            "lua", "vim", "vimdoc", "query",
-            "markdown", "markdown_inline",
-            "toml", "yaml", "json", "bash",
-        }
-
-        local installed = require("nvim-treesitter").get_installed()
-        local to_install = vim.tbl_filter(function(lang)
-            return not vim.tbl_contains(installed, lang)
-        end, ensure)
-
-        if #to_install > 0 then
-            vim.cmd("TSInstall " .. table.concat(to_install, " "))
-        end
+        require("nvim-treesitter.configs").setup({
+            ensure_installed = {
+                "python", "rust", "c_sharp", "gdscript",
+                "lua", "vim", "vimdoc", "query",
+                "markdown", "markdown_inline",
+                "toml", "yaml", "json", "bash",
+            },
+            highlight = { enable = true },
+        })
     end,
 }
